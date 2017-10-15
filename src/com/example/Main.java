68fa19327d3998438d259656820164319d8a4367
Brian Sheen <bsheen@gmail.com>
2017-10-15 17:43:54 -0500

    vertical scan solution

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,22 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] strs = {"lee", "lees", "leetcode", "lets", "leeee"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); ++i) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; ++j) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
     }
 }
